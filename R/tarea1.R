2442a697f09bd341c2995243851d1df5f8b313ac
maria.bermeoa@gmail.com <maria.bermeoa@gmail.com>
2022-07-19 15:39:48 -0500

    Tarea 1 finalizada No pude cambiar el colo de las barras

--- R/tarea1.R
+++ R/tarea1.R
@@ -7,3 +7,22 @@ library(tidyverse)
 iris
 
 class(iris)
+
+ggplot(data = iris) +
+  geom_histogram(mapping = aes(x = Petal.Length,
+                               fill = Species,
+                               color = Species)) +
+  theme_minimal()+
+  labs(x = "Longitud del petalo", 
+       y = "Frecuencia",
+       title = "Frecuencia de longitud del petalo por especie de Iris")+
+  theme(plot.title = element_text(hjust=0.8),
+        text = element_text(size = 16))
+
+
+
+
+
+plot.new()          
+
+                                 
