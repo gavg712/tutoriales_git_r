96fd2cefbc5646820a9784090281dee1bec59e34
gavg712 <gavg712@gmail.com>
2022-07-20 15:42:19 -0500

    Revisado de Tarea 1 Elizabeth

--- R/Tarea 1.R
+++ R/Tarea 1.R
@@ -1,10 +1,14 @@
-iris 
 library(tidyverse)
 
-ggplot(data=iris)+geom_histogram( mapping = aes(x=Petal.Width, fill=Species))+
-  theme_classic()+ labs(x="Ancho del Pétalo", y= "Frecuencia", title= "Histograma de frecuencia
-                       de Ancho de Pétalo por especie de iris")+
-                              theme(plot.title=element_text(hjust=0.7))+theme(text = element_text(size=17))
+ggplot(data = iris) + 
+    geom_histogram( mapping = aes(x = Petal.Width, fill = Species)) +
+    theme_classic() + 
+    labs(x = "Ancho del Pétalo", 
+         y = "Frecuencia", 
+         title = "Ancho de Pétalo por especie de iris",
+         subtitle = "Frecuencias absolutas") +
+    theme(plot.title = element_text(hjust = 0.7),
+          text = element_text(size=17))
 
      
 
